f34e13e62fb58b818bb7ebe22207f59bbb2c9191
Oleg Zagryvyi <olegzagraf@gmail.com>
2024-05-13 19:01:36 +0300

    fix unique username&email

--- postgres/sql/authdb.sql
+++ postgres/sql/authdb.sql
@@ -1,9 +1,9 @@
 CREATE TABLE users
 (
     id       SERIAL PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
+    username VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    email    VARCHAR(255) NOT NULL,
+    email    VARCHAR(255) UNIQUE NOT NULL,
     role     VARCHAR(20)  NOT NULL
 );
 
